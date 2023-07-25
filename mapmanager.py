350219be654ab4b1b8feaa292a999c5a21c7ed2b
Ali Messaoudi <83890004+MESSDEV@users.noreply.github.com>
2023-07-25 12:31:44 +0200

    Update mapmanager.py

--- mapmanager.py
+++ mapmanager.py
@@ -1,25 +1,58 @@
 class Mapmanager():
-   """ Managing map """
+   """Map management"""
    def __init__(self):
        self.model = 'block' # the cube model is in the block.egg file
-       # the following textures are used: 
-       self.texture = 'block.png'        
-       self.color = (0.2, 0.2, 0.35, 1) #rgba
-
-
+       # # the following textures are used:
+       self.texture = 'block.png'         
+       self.colors = [
+           (0.2, 0.2, 0.35, 1),
+           (0.2, 0.5, 0.2, 1),
+           (0.7, 0.2, 0.2, 1),
+           (0.5, 0.3, 0.0, 1)
+       ] #rgba
        # create the main map node:
        self.startNew()
-        # create building blocks   
-       self.addBlock((0,10, 0))
+       # self.addBlock((0,10, 0))
 
 
    def startNew(self):
-       """ creating a base for new map """
-       self.land = render.attachNewNode("Land") # the node which all the map blocks are attached to
-  
+       """creates the basis for the new map"""
+       self .land = render.attachNewNode ( "Land" ) #the node which all the map blocks are attached to
+
+
+   def getColor(self, z):
+       if z < len(self.colors):
+           return self.colors[z]
+       else:
+           return self.colors[len(self.colors) - 1]
+
+
    def addBlock(self, position):
+       # create building blocks
        self.block = loader.loadModel(self.model)
        self.block.setTexture(loader.loadTexture(self.texture))
        self.block.setPos(position)
+       self.color = self.getColor(int(position[2]))
        self.block.setColor(self.color)
        self.block.reparentTo(self.land)
+
+
+   def clear(self):
+       "" "resets the map" ""
+       self.land.removeNode()
+       self.startNew()
+
+
+   def loadLand(self, filename):
+       "" "creates a land map from a text file, returns its dimensions" ""
+       self.clear()
+       with open(filename) as file:
+           y = 0
+           for line in file:
+               x = 0
+               line = line.split(' ')
+               for z in line:
+                   for z0 in range(int(z)+1):
+                       block = self.addBlock((x, y, z0))
+                   x += 1
+               y += 1
